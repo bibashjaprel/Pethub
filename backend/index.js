03b46cff0d78405eaa651fc5a5d04bd98c601bbf
Bibash Japrel <bibashjaprel111@gmail.com>
2024-09-13 07:45:42 +0545

    DB configuration

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./dbconfig/dbconfig.js')
+connectDB()
 require('dotenv').config()
 const PORT = process.env.PORT
 
 app.get('/login',(req, res)=>{
   res.send("Hello I'm Login Page")
 })
+
 app.listen(PORT, ()=>{
   console.log(`I am Live at ${PORT}`)
 })
