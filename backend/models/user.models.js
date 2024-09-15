dd4d72463f1383d104547f82dcab82625562430c
Bibash Japrel <bibashjaprel111@gmail.com>
2024-09-15 11:52:03 +0545

    define user models

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,6 +1,31 @@
 import mongoose, { mongo } from "mongoose";
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  }
+}, {timestamps: true})
 
 export const User = mongoose.model("User", userSchema)
 
