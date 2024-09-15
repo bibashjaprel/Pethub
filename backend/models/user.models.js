8a69eea8ca0652a38440688ecc04383e6e982c76
Bibash Japrel <bibashjaprel111@gmail.com>
2024-09-15 16:30:30 +0545

    validation

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,4 +1,6 @@
-import mongoose, { mongo } from "mongoose";
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -27,5 +29,48 @@ const userSchema = new mongoose.Schema({
   }
 }, {timestamps: true})
 
-export const User = mongoose.model("User", userSchema)
+
+//static signup method
+userSchema.statics.signup = async function (email, password){
+  if(!email || !password){
+      throw Error('All fields are required!')
+  }
+  if(!validator.isEmail(email)){
+      throw Error('Email is not valid')
+  }
+  const exists = await this.findOne({email})
+  if(exists){
+      throw Error('Email already exist')
+  }
+
+  if(!validator.isStrongPassword(password)){
+      throw Error ('Password not strong enough')
+  }
+  //hashing the password
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+  const user = await this.create({email,password: hash})
+  console.log(user)
+  return user
+}
+
+//static login method
+userSchema.statics.login = async function(email, password){
+  if(!email || !password){
+      throw Error('All fields are required!')
+  }
+  const user = await this.findOne({email})
+
+  if(!user){
+      throw Error('Please register your account ')
+  }
+const match = await bcrypt.compare(password,user.password)
+if(!match){
+  throw Error('Incorrect password')
+}
+return user
+}
+
+
+module.exports = mongoose.model("User", userSchema)
 
