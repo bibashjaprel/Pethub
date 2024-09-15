0f0de63329839ebdbd749cfa79828e70024681d1
Bibash Japrel <bibashjaprel111@gmail.com>
2024-09-15 16:33:13 +0545

    creating user controlller with jwt token

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,37 @@
+const User = require('../models/user.models.js')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) =>{
+    return jwt.sign({_id},process.env.SECRET,{expiresIn:'30d'})
+}
+
+const loginUser = async (req, res)=>{
+  const {email, password} = req.body
+
+  try{
+    const user = await User.login(email, password)
+
+    //creating a token
+    const token = createToken(user._id)
+    res.status(200).json({email, token})
+  }catch(error){
+    res.status(400).json({error: error.message})
+  }
+}
+
+
+const signupUser = async (req, res)=>{
+  const {email, password} = req.body
+
+  try{
+    const user = await User.signup(email, password)
+
+    //creating a token
+    const token = createToken(user._id)
+    res.status(200).json({email, token})
+  }catch(error){
+    res.status(400).json({error: error.message})
+  }
+}
+
+module.exports = {loginUser, signupUser}
