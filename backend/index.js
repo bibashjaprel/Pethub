4e93136324a5ccf9e4329cc00343312de22efe64
Bibash Japrel <bibashjaprel111@gmail.com>
2024-09-15 13:39:54 +0545

    basic routes config

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,16 @@ connectDB()
 require('dotenv').config()
 const PORT = process.env.PORT
 
+const userRoutes = require('./routes/user.routes.js')
+const petRoutes = require('./routes/pet.routes.js')
+
 app.get('/login',(req, res)=>{
   res.send("Hello I'm Login Page")
 })
+
+//routes
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/pet', petRoutes);
 
 app.listen(PORT, ()=>{
   console.log(`I am Live at ${PORT}`)
