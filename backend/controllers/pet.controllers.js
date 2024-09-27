1fd1a6742485b132e807d52cedd9b5d02e5b019c
Bibash Japrel <bibashjaprel111@gmail.com>
2024-09-27 19:45:07 +0545

    spelling check

--- backend/controllers/pet.controllers.js
+++ backend/controllers/pet.controllers.js
@@ -9,7 +9,7 @@ const getPets = async (req, res)=>{
 
 // get single pet
 const getPet = async (req, res)=>{
-  const {id} = req.prams
+  const {id} = req.params
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({error: 'No such pet'})
   }
@@ -29,10 +29,10 @@ const createPet = async (req, res) => {
   let emptyFields = []
 
   if(!name){
-    emptyFields.push('title')
+    emptyFields.push('name')
   }
   if(!species){
-    emptyFields.push('title')
+    emptyFields.push('species')
   }
   if(!breed){
     emptyFields.push('breed')
@@ -51,7 +51,7 @@ const createPet = async (req, res) => {
   //adding to the database
   try{
     const pet = await Pet.create({name, species, breed, age, description})
-    res.status(200).json(pet)
+    res.status(201).json(pet)
   }catch(error){
     res.status(400).json({error: error.message})
   }
@@ -60,7 +60,7 @@ const createPet = async (req, res) => {
 
 //delete pet
 const deletePet = async (req, res) => {
-  const { id } = req.prams
+  const { id } = req.params
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({error: 'No such pet'})
   }
@@ -75,7 +75,7 @@ const deletePet = async (req, res) => {
 
 // update pet 
 const updatePet = async (req, res) => {
-  const { id } = req.prams
+  const { id } = req.params
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({error: 'No such Pet'})
   }
