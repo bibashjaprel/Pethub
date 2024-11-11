b63c47d4ac24ed0cf839636b1bcb820b1d31afc0
Bibash Japrel <bibashjaprel111@gmail.com>
2024-11-11 23:14:53 +0545

    midddleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const {loginUser,signupUser, getUsers, getUser, deleteUser} = require('../controllers/user.controllers.js')
+const authMiddleware = require('../middlewares/authMiddleware.js');
 
 console.log(getUser)
 
 //auth routes
 router.post('/login', loginUser)
-router.post('/signup', signupUser)
+router.post('/signup',  signupUser)
 
 //user routes
-router.get('/users', getUsers)
-router.get('/:id', getUser)
-router.delete('/delete/:id', deleteUser)
+router.get('/users' , authMiddleware, getUsers)
+router.get('/:id', authMiddleware, getUser)
+router.delete('/delete/:id', authMiddleware, deleteUser)
 
 
 module.exports = router;
