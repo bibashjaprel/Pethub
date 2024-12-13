a4e2be7041f8681e5fa364e42ae401e45222bbd5
Bibash Japrel <bibashjaprel111@gmail.com>
2024-12-13 22:59:05 +0545

    comments

--- backend/routes/petAdoption.routes.js
+++ backend/routes/petAdoption.routes.js
@@ -7,17 +7,21 @@ const {
   createAdoptionRequest,
   deleteAdoptionRequest,
   updateAdoptionRequest
-} = require('../controllers/petAdoption.cotrollers');
+} = require('../controllers/petAdoption.controllers');
 
-
+// Get all adoption requests
 router.get('/', getAdoptionRequests);
 
+// Get a specific adoption request by ID
 router.get('/:id', getAdoptionRequest);
 
+// Create a new adoption request
 router.post('/', createAdoptionRequest);
 
+// Delete a specific adoption request by ID
 router.delete('/:id', deleteAdoptionRequest);
 
+// Update a specific adoption request by ID
 router.patch('/:id', updateAdoptionRequest);
 
 module.exports = router;
