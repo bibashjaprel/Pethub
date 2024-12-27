b64fc1d42922c5b53b5c19ad24a0fe9d63d5520e
Bibash Japrel <bibashjaprel111@gmail.com>
2024-12-28 00:07:05 +0545

    auth middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ module.exports = async (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.SECRET); 
+    const decoded = jwt.verify(token, process.env.SECRET);
     const user = await User.findById(decoded._id);
     if (!user) {
       return res.status(401).json({ message: 'Invalid user or token expired.' });
